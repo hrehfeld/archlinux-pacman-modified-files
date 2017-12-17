abfb5813b6fcaec044d1ae024577e55bf8788f4a
hrehfeld <hauke@haukerehfeld.de>
2017-12-17 11:08:13 +0100

    chg: better sync

--- sync.sh
+++ sync.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
+etc_repo=/home/hrehfeld/etc-repo
+set -e
 if [[ ! -e $etc_repo/.synced ]]
 then
 	cd $etc_repo
-	for f in $(git ls-tree -r  "!$HOST" --name-only | ag -wv branches)
+	for f in $(git ls-tree -r "!$(hostname)" --name-only --full-name | ag -wv branches)
 	do
 		[[ -e /$f ]] && cp -a /$f /$f.bak
 		cp -a $f /$f
