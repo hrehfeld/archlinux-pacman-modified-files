b2f5ca4b099d971316459e382bb0d91c0ba3746a
hrehfeld <hauke@haukerehfeld.de>
2017-12-28 21:01:00 +0100

    chg: sync supports hg

--- sync.sh
+++ sync.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env sh
-etc_repo=/home/hrehfeld/etc-repo
 set -e
+set -x
+etc_repo=/home/hrehfeld/etc-$(hostname)
+backup_path=/home/hrehfeld/etc-backup
+list_files() {
+	#git ls-tree -r "!$(hostname)" --name-only --full-name
+	hg status -nA
+}
 if [[ ! -e $etc_repo/.synced ]]
 then
 	cd $etc_repo
-	for f in $(git ls-tree -r "!$(hostname)" --name-only --full-name | ag -wv branches)
+	for f in $(list_files  | ag -wv branches)
 	do
-		[[ -e /$f ]] && cp -a /$f /$f.bak
-		cp -a $f /$f
+		orgp="/$f"
+		repop="$etc_repo/$f"
+		backupp="$backup_path/$f"
+		[[ -e $orgp ]] && mkdir -p $backupp && cp -a $orgp $backupp
+		cp -a $repop $orgp
 	done
 fi
