87fea7361a03b315393ef447d34c5662080abd9d
hrehfeld <hauke@haukerehfeld.de>
2018-08-12 16:09:29 +0200

    cln: hg to own file

--- pacutil/__main__.py
+++ pacutil/__main__.py
@@ -37,6 +37,9 @@ from . import color as col
 from .util import temp_dir, mkdir_p, check_call, check_output, copy_archive, file_hash, get_hash, handle_filepath, clean_glob
 from .util import chmod, filter_odict, startswith_any, is_system_file, natural_comp, ListComp, split_lines
 from .util import hostname as machine
+from .hg import hg as _hg
+
+hg = lambda repo_path: _hg(repo_path, log=log)
 
 with Path('.pkg-blacklist').open('r') as f:
     pkg_blacklist = [p.strip() for p in f.read().split('\n')]
@@ -378,99 +381,6 @@ def get_orphan_pkgs():
         r[f] = pkg
     return r
 
-class hg:
-    class HgException(Exception):
-        pass
-
-    @staticmethod
-    def is_repo_internal_dir(p):
-        return p.exists() and p.is_dir() and p.name == '.hg'
-
-    def __init__(self, repo_path):
-        self.repo_path = repo_path
-
-    def __getattr__(self, name):
-        def f(*args, **kwargs):
-            args = [str(a) for a in args]
-            kws = []
-            for k, v in kwargs.items():
-                if v is False:
-                    continue
-                
-                if len(k) > 1:
-                    n = '--' + k
-                else:
-                    n = '-' + k
-                kws.append(n)
-                if v not in [True, None]:
-                    kws.append(str(v))
-                    
-            cmd = ['hg', name, *kws, *args]
-            log.info(self.repo_path + ': ' + ' '.join(cmd))
-            try:
-                r = check_output(cmd, cwd=self.repo_path, universal_newlines=True, bufsize=16384 * 16)
-            except subprocess.CalledProcessError as e:
-                raise hg.HgException(str(e))
-            return r
-        return f
-
-    def empty_commit(self, msg):
-        t = Path(self.repo_path) / '.empty'
-        t.write_text('')
-        self.add(t)
-        self.commit(m=msg)
-        self.rm(t)
-        self.commit(m=msg, amend=True)
-
-    def commit_merge(self, branch, cur=None):
-        if not cur:
-            cur = self.branch().strip()
-        if split_lines(self.merge(branch, P=True)):
-            self.merge(branch)
-            self.commit(m='mrg: %s into %s' % (branch, cur))
-
-    def has_branch(self, branch):
-        return branch in split_lines(self.branches(q=True))
-
-    def initialize(self):
-        repo_path = Path(self.repo_path) / '.hg'
-        if not self.is_repo_internal_dir(repo_path):
-            mkdir_p(repo_path)
-            self.init()
-
-    def commit_and_tag(self, files, msg, tag):
-        self.add(*files)
-
-        if self.diff():
-            self.commit(*files, m=msg)
-
-            #reassign tag
-            self.tag(tag, local=True, force=True)
-
-
-    def repo_files(self):
-        r = self.status(A=True)
-        ls = split_lines(r)
-        ls = [l.split(' ', 1)[1] for l in ls if not l.startswith('?')]
-        return ls
-
-    
-            
-
-    def ensure_branch(self, name, from_branch=None, commit=True, clean=False):
-        if not self.has_branch(name):
-            if from_branch:
-                self.update(from_branch, clean=clean)
-            elif clean:
-                self.update(clean=True)
-            self.branch(name)
-            if commit:
-                self.empty_commit('initial')
-            return True
-        else:
-            self.update(name, clean=clean)
-            return False
-
 
 def get_file_org(pkg, version, files, outdir, is_aur):
     chroot_path = CHROOT_PATH / 'org' / pkg
@@ -523,7 +433,10 @@ def machine_branch(pkg):
 def machine_branch_main():
     return MACHINE_SEP + machine
 
-class PkgRepo(hg):
+class PkgRepo(_hg):
+    def __init__(self, *args):
+        _hg.__init__(self, *args, log=log)
+
 
     def files_differ(self, fs, integrity_check=False):
         #check if all files are present
---
+++ pacutil/hg.py
@@ -0,0 +1,101 @@
+import subprocess
+from pathlib import Path
+
+from .util import split_lines
+
+class hg:
+    class HgException(Exception):
+        pass
+
+    @staticmethod
+    def is_repo_internal_dir(p):
+        return p.exists() and p.is_dir() and p.name == '.hg'
+
+    def __init__(self, repo_path, log=None):
+        self.repo_path = repo_path
+        self.log = log
+
+    def __getattr__(self, name):
+        def f(*args, **kwargs):
+            args = [str(a) for a in args]
+            kws = []
+            for k, v in kwargs.items():
+                if v is False:
+                    continue
+                
+                if len(k) > 1:
+                    n = '--' + k
+                else:
+                    n = '-' + k
+                kws.append(n)
+                if v not in [True, None]:
+                    kws.append(str(v))
+                    
+            cmd = ['hg', name, *kws, *args]
+            if self.log:
+                self.log.info(self.repo_path + ': ' + ' '.join(cmd))
+            try:
+                r = subprocess.check_output(cmd, cwd=self.repo_path, universal_newlines=True, bufsize=16384 * 16)
+            except subprocess.CalledProcessError as e:
+                raise hg.HgException(str(e))
+            return r
+        return f
+
+    def empty_commit(self, msg):
+        t = Path(self.repo_path) / '.empty'
+        t.write_text('')
+        self.add(t)
+        self.commit(m=msg)
+        self.rm(t)
+        self.commit(m=msg, amend=True)
+
+    def commit_merge(self, branch, cur=None):
+        if not cur:
+            cur = self.branch().strip()
+        if split_lines(self.merge(branch, P=True)):
+            self.merge(branch)
+            self.commit(m='mrg: %s into %s' % (branch, cur))
+
+    def has_branch(self, branch):
+        return branch in split_lines(self.branches(q=True))
+
+    def initialize(self):
+        repo_path = Path(self.repo_path) / '.hg'
+        if not self.is_repo_internal_dir(repo_path):
+            mkdir_p(repo_path)
+            self.init()
+
+    def commit_and_tag(self, files, msg, tag):
+        self.add(*files)
+
+        if self.diff():
+            self.commit(*files, m=msg)
+
+            #reassign tag
+            self.tag(tag, local=True, force=True)
+
+
+    def repo_files(self):
+        r = self.status(A=True)
+        ls = split_lines(r)
+        ls = [l.split(' ', 1)[1] for l in ls if not l.startswith('?')]
+        return ls
+
+    
+            
+
+    def ensure_branch(self, name, from_branch=None, commit=True, clean=False):
+        if not self.has_branch(name):
+            if from_branch:
+                self.update(from_branch, clean=clean)
+            elif clean:
+                self.update(clean=True)
+            self.branch(name)
+            if commit:
+                self.empty_commit('initial')
+            return True
+        else:
+            self.update(name, clean=clean)
+            return False
+
+
