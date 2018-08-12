9a5ef70cd7ac94ce9887f4e83d66b48a8302aae8
hrehfeld <hauke@haukerehfeld.de>
2018-08-12 15:42:37 +0200

    add: more log abstraction

--- pacutil/logging.py
+++ pacutil/logging.py
@@ -3,21 +3,37 @@ from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
 
 try:
     import coloredlogs
-    coloredlogs.install()
 except ImportError:
     pass
 
+DEBUG = 'debug'
+INFO = 'info'
+WARNING = 'warning'
+ERROR = 'error'
+CRITICAL = 'critical'
+
+_loglevels = { DEBUG: logging.DEBUG, INFO: logging.INFO, WARNING: logging.WARNING, ERROR: logging.ERROR, CRITICAL: logging.CRITICAL }
+
 # can only import these after basicConfig is set
 message = lambda *args: print(*args)
-debug, info, warning, error, critical = logging.debug, logging.info, logging.warning, logging.error, logging.critical
+debug = info = lambda *args: None
+warning = error = critical = lambda *args: print(*args)
 
 def init(quiet, log_level):
+    global message, debug, info, warning, error, critical
     if quiet:
-        log_level = logging.WARNING
+        print('quiet mode')
+        log_level = WARNING
         message = debug
-        
-    logging.basicConfig(level=log_level)
+
+    print(log_level)
+
+    log_level = _loglevels[log_level]
+
+    logging.basicConfig(level=log_level + 1)
+    coloredlogs.install()
     # import after basicConfig
+
     debug, info, warning, error, critical = logging.debug, logging.info, logging.warning, logging.error, logging.critical
 
     
